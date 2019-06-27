Fix Bregman Divergence computation

experiments/clustering.py
@@ -13,15 +13,22 @@ from sklearn.preprocessing import LabelEncoder
 from os.path import join
 import pandas as pd
 
+VERBOSE = True
+
 def project_with_sparse_zigap(counts, k=2):
-    model = SparseZIGaP(counts, k=k, use_factors=True)
+    model = SparseZIGaP(counts, k=k, use_factors=False)#True)
     best_divergence = model.reconstruction_deviance()
-    print('Initial Bregman divergence: %f' % best_divergence)
+    if VERBOSE:
+        print('Initial Bregman divergence: %f' % best_divergence)
     U, V = model.factors()
     for iteration in range(200):
         model.step()
         divergence = model.reconstruction_deviance()
-        print('Iteration %3i - Bregman divergence: %f' % (iteration + 1, divergence))
+        exp_deviance = model.explained_deviance()
+        print('%dev:', exp_deviance)
+        if VERBOSE:
+            print('Iteration %3i - Bregman divergence: %f' % (iteration + 1, divergence))
+            print('\t\tVariance U:', model.factors()[0].std()**2)
         if divergence <= best_divergence:
             best_divergence = divergence
             U, V = model.factors()
@@ -31,39 +38,41 @@ def project_with_sparse_zigap(counts, k=2):
     return U, V
 
 
-def test_generated():
+def test_on_generated_dataset(K, theta, plot=False):
     # --- Generate random cells and genes ---
 
     zero_inflation_level = 0.5
     n_groups = 2
-    n, m, k = 100, 800, 40
+    n, m, k = 100, 800, K
     X, _, _, labels = generate_factor_matrices(
             n, m, k,
             sparsity_degree_in_v=0.9,
             beta=80,
-            theta=0.5, # Degree of separation between clusters
+            theta=theta, # Degree of separation between clusters
             n_groups=n_groups, # Number of clusters
             zero_inflation_level=zero_inflation_level)
     counts = CountMatrix(X)
 
-    plt.imshow(X)
-    plt.title('Synthetic count matrix')
-    plt.xlabel('Cells')
-    plt.ylabel('Genes')
-    plt.show()
+    if plot:
+        plt.imshow(X)
+        plt.title('Synthetic count matrix')
+        plt.ylabel('Genes')
+        plt.xlabel('Cells')
+        plt.show()
 
 
     # --- Clustering ---
 
-    U, V = project_with_sparse_zigap(counts, k=40)
+    U, V = project_with_sparse_zigap(counts, k=k)
     log_U, log_V = np.log(U), np.log(V)
 
     predicted_labels = KMeans(n_clusters=n_groups, n_init=100).fit(log_U).labels_
 
-    ari = adjusted_rand_score(labels, predicted_labels)
-    print(labels)
-    print(predicted_labels)
-    print('Adjusted Rand Index: %f' % ari)
+    ari_k = adjusted_rand_score(labels, predicted_labels)
+    if VERBOSE:
+        print(labels)
+        print(predicted_labels)
+    print('Adjusted Rand Index (k): %f' % ari_k)
 
 
     # --- Visualization ---
@@ -71,20 +80,59 @@ def test_generated():
     U, V = project_with_sparse_zigap(counts, k=2)
     log_U, log_V = np.log(U), np.log(V)
     predicted_labels = KMeans(n_clusters=n_groups, n_init=100).fit(log_U).labels_
+    ari_2 = adjusted_rand_score(labels, predicted_labels)
+    print('Adjusted Rand Index (2): %f' % ari_2)
 
-    colors = ['salmon', 'steelblue', 'purple', 'gold']
-    markers = ['o', '^', 'x', 'P']
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for i in range(n_groups):
-        for j in range(n_groups):
-            idx = np.logical_and(labels == i, predicted_labels == j)
-            ax.plot(U[idx, 0], U[idx, 1], color=colors[i], marker=markers[j], label=(i,j), lw=0, ms=8)
-    ax.set_title('Projection of synthetic data with Sparse ZIGaP ($k=2$)')
-    ax.set_ylabel(r'Factor $\widehat{U_2}$')
-    ax.set_xlabel(r'Factor $\widehat{U_1}$')
-    ax.legend()
-    plt.show()
+    if plot:
+        colors = ['salmon', 'steelblue', 'purple', 'gold']
+        markers = ['o', '^', 'x', 'P']
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for i in range(n_groups):
+            for j in range(n_groups):
+                idx = np.logical_and(labels == i, predicted_labels == j)
+                ax.plot(log_U[idx, 0], log_U[idx, 1], color=colors[i], marker=markers[j], label=(i,j), lw=0, ms=8)
+        ax.set_title('Projection of synthetic data with Sparse ZIGaP ($k=2$)')
+        ax.set_ylabel(r'Factor $\widehat{U_2}$')
+        ax.set_xlabel(r'Factor $\widehat{U_1}$')
+        ax.legend()
+        plt.show()
+    return ari_k, ari_2
+
+def test_generated(repeat=False):
+    if repeat:
+        K = 10
+        global VERBOSE
+        VERBOSE = False
+        ari_k = list()
+        ari_2 = list()
+        thetas = np.linspace(0, 1, 3)
+        for theta in thetas:
+            print('\t\t-- \\theta =', theta, '--')
+            ari_k.append(list())
+            ari_2.append(list())
+            for i in range(20):
+                aris = test_on_generated_dataset(K, theta, plot=False)
+                ari_k[-1].append(aris[0])
+                ari_2[-1].append(aris[1])
+        ari_k = np.array(ari_k)
+        ari_2 = np.array(ari_2)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.errorbar(thetas, ari_k.mean(axis=1), yerr=ari_k.std(axis=1), label='ARI ({})'.format(K), capsize=6, marker='*', ms=8)
+        ax.errorbar(thetas, ari_2.mean(axis=1), yerr=ari_2.std(axis=1), label='ARI (2)', capsize=6, marker='*', ms=8)
+        ax.legend()
+        ax.set_xlabel(r'$\theta$')
+        ax.set_ylabel('ARI')
+        ax.grid()
+        ax.legend(loc='upper left')
+        plt.show()
+        #print(ari_k)
+        #print(ari_2)
+        #print(np.mean(ari_k), np.mean(ari_2))
+        #print(np.std(ari_k), np.std(ari_2))
+    else:
+        test_on_generated_dataset(10, .5, plot=True)
 
 def get_data_path(file_name):
     return join('../../data/', file_name)
@@ -99,11 +147,11 @@ def test_dataset():
     counts.filter_rows(indices, inplace=True)
     labels = LabelEncoder().fit_transform(cells_and_types['type'])
     assert labels.shape[0] == counts.shape[0], (labels.shape, counts.shape)
-    U, V = project_with_sparse_zigap(counts, k=40)
-    predicted_labels = KMeans(n_clusters=np.unique(labels).shape[0], n_init=100).fit(U).labels_
+    U, V = project_with_sparse_zigap(counts, k=10)
+    predicted_labels = KMeans(n_clusters=np.unique(labels).shape[0], n_init=100).fit(log_U).labels_
     ari = adjusted_rand_score(labels, predicted_labels)
     print('Adjusted Rand Index:', ari)
 
 if __name__ == '__main__':
-    #test_generated()
-    test_dataset()
+    test_generated(repeat=False)
+    #test_dataset()

main.py
@@ -10,28 +10,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-ROOT = os.path.dirname(os.path.abspath(__file__))
+#ROOT = os.path.dirname(os.path.abspath(__file__))
+ROOT = '../'
 DATA_FOLDER = os.path.join(ROOT, 'data')
 
+def frobenius(X):
+    return np.sqrt((X**2).sum())
 
 if __name__ == '__main__':
 
 
     filepath = os.path.join(DATA_FOLDER, 'llorens.csv')
-    counts = CountMatrix.from_csv(filepath).T
+    counts = CountMatrix.from_csv(filepath)#.T
+    X = counts.as_array()
     print('Shape of X: %s' % str(counts.shape))
 
     history = list()
     model = SparseZIGaP(counts, k=2, use_factors=True)
     best_divergence = model.reconstruction_deviance()
-    print('Initial Bregman divergence: %f' % best_divergence)
-    history.append(best_divergence)
     U, V = model.factors()
+    Lambda = np.dot(U, V.T)
+    kappa = (X*Lambda).sum() / (Lambda**2).sum()
+    print('Initial Bregman divergence: %f' % best_divergence)
+    print('Initial Frobenius distance: %f' % frobenius(X - kappa*Lambda))
+    history.append(best_divergence)
     for iteration in range(50):
-        print(np.round(np.dot(U, V.T)).astype(np.int))
+        Lambda = np.dot(U, V.T)
+        #Lambda = np.round(Lambda).astype(np.int)
+        kappa = (X*Lambda).sum() / (Lambda**2).sum()
         model.step()
         divergence = model.reconstruction_deviance()
-        print('Iteration %i - Bregman divergence: %f' % (iteration + 1, divergence))
+        print('Iteration %3i - Bregman divergence: %f' % (iteration + 1, divergence))
+        print('              - Frobenius distance: %f' % frobenius(X - kappa*Lambda))
 
         if True:#divergence <= best_divergence:
             best_divergence = divergence

oriana/models/base.py
@@ -56,29 +56,29 @@ class FactorModel(metaclass=ABCMeta):
         self.update_prior_hyper_parameters() # M-step
 
     def reconstruction_deviance(self):
-        """
         self.UV[:] = self.X[:]
-        ll_X_given_X = self.X.loglikelihood()
+        self.D[:] = np.round(self.D_hat)
+        mask = self.D[:] == 0
+        assert not mask.all()
+        ll_X_given_X = self.loglikelihood_X()
         self.U[:] = self.U_hat
-        self.V[:] = self.V_hat
+        self.V[:] = self.Vprime_hat * self.S_hat
         self.UV.forward()
-        ll_X_given_UV = self.X.loglikelihood()
+        self.UV[mask] = 0
+        ll_X_given_UV = self.loglikelihood_X()
         return -2. * (ll_X_given_UV - ll_X_given_X)
-        """
-        X = self.X[:]
-        Lambda = self.UV[:]
-        A = np.empty_like(X)
-        #Lambda[Lambda == 0] = 1e-15 # TODO
-        A = np.nan_to_num(X * np.log(X / Lambda))
-        return (A - X + Lambda).sum()
 
     def explained_deviance(self):
+        mask = self.D[:] == 0
+        assert not mask.all()
         self.UV[:] = self.X[:]
-        ll_X_given_X = self.X.loglikelihood()
-        self.UV[:] = self.X[:].mean(axis=1)[..., np.newaxis] # TODO: not sure about the axis
-        ll_X_given_X_mean = self.X.loglikelihood()
+        ll_X_given_X = self.loglikelihood_X()
+        self.UV[:] = self.X[:].mean(axis=0)[np.newaxis, ...]
+        ll_X_given_X_mean = self.loglikelihood_X()
         self.UV.forward()
-        ll_X_given_UV = self.X.loglikelihood()
+        self.UV[mask] = 0
+        ll_X_given_UV = self.loglikelihood_X()
+        assert (ll_X_given_X >= ll_X_given_X_mean).all()
         return (ll_X_given_UV - ll_X_given_X_mean) / (ll_X_given_X - ll_X_given_X_mean)
 
     def frobenius_norm(self):

oriana/nodes/probabilistic/poisson.py
@@ -19,7 +19,7 @@ class Poisson(ProbabilisticNode):
 
     def __init__(self, l, rel, **kwargs):
         """Constructs a Poisson node.
-        
+
         Parameters:
             l (object): Node or Parameter object representing
                 the average number of event occurences in a fixed
@@ -42,7 +42,7 @@ class Poisson(ProbabilisticNode):
         """
         Parameters:
             l (object): Poisson lambda parameter, or the average
-                number of event occurences in a fixed interval of time.
+                number of event occurrences in a fixed interval of time.
                 Can be either a Parameter or a Node object.
         """
         n = self.n_samples_per_distrib
@@ -62,6 +62,12 @@ class Poisson(ProbabilisticNode):
         return out
 
     def _logp(self, samples, l):
+        assert (l>=0).all()
         l = l.reshape(-1, order='C')
-        logps = -l + samples * log(l) + log(factorial(np.floor(samples)))
-        return logps
+        samples = samples.reshape(-1, order='C')
+        logps = np.empty_like(samples)
+        logps[np.logical_and(samples == 0, l == 0)] = 0
+        logps[np.logical_and(samples > 0, l == 0)] = -1000  # TODO: This is not very clean
+        logps[l > 0] = -l[l > 0] + samples[l > 0] * log(l[l > 0])
+        #logps = -l + samples * log(l) ##- log(factorial(np.floor(samples)))
+        return np.nan_to_num(logps)

oriana/singlecell/generation.py
@@ -10,8 +10,8 @@ def generate_u(n, k, n_groups=3, theta=0.5):
             * n_groups, n // n_groups)) + [n]
     block_col_indices = list(range(0, (k // n_groups) \
             * n_groups, k // n_groups)) + [k]
-    
-    alpha = np.random.choice([100, 250], size=n_groups)
+
+    alpha = np.random.choice([100, 250], size=n_groups) / k
     EMPTY = -1
     U = np.full((n, k), EMPTY, dtype=np.float)
 
@@ -28,12 +28,12 @@ def generate_u(n, k, n_groups=3, theta=0.5):
         block_width = j_end - j_start
         block_size = (block_height, block_width)
 
-        B_g = np.random.gamma(1., 1. / alpha[g], size=block_size)
+        B_g = np.random.gamma(1., alpha[g], size=block_size)
         U[i_start:i_end, j_start:j_end] = B_g
 
     alpha_bar = np.mean(alpha)
     U[U == EMPTY] = np.random.gamma(
-            1., 1. / ((1. - theta) * alpha_bar), size=(n, k))[U == EMPTY]
+            1., (1. - theta) * alpha_bar, size=(n, k))[U == EMPTY]
     return U, labels
 
 
@@ -57,11 +57,11 @@ def generate_v(m, k, sparsity_degree=0.2, beta=80, theta=0.8, n_groups=2):
         block_width = j_end - j_start
         block_size = (block_height, block_width)
 
-        B_g = np.random.gamma(1., 1. / beta, size=block_size)
+        B_g = np.random.gamma(1., beta, size=block_size)
         V[i_start:i_end, j_start:j_end] = B_g
 
     V[V == EMPTY] = np.random.gamma(
-            1., 1. / ((1. - theta) * beta), size=(m, k))[V == EMPTY]
+            1., (1. - theta) * beta, size=(m, k))[V == EMPTY]
     return V
 
 
@@ -83,4 +83,4 @@ def generate_factor_matrices(n, m, k, sparsity_degree_in_v=0.5,
     ones = np.ones(m, dtype=np.int)
     D = np.random.binomial(ones, pi_d, size=(n, m))
     X = D * Lambda
-    return X, U, V, labels
+    return X.astype(np.int), U, V, labels

oriana/utils.py
@@ -16,7 +16,8 @@ def sigmoid(x):
 
 
 def log(x):
-    return np.log(np.maximum(1e-15, x))
+    tmp = np.maximum(1e-15, x)
+    return np.log(tmp, out=tmp)
 
 
 def factorial(x):